195d9dec567d8513b70fda0ac4fa204b968b6a68
vimster <mr.potsdam@gmx.de>
2015-04-16 19:20:14 +0200

    http requests

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,18 @@
 module Main where
 
--- addNumbers :: Integer -> Integer -> Integer
--- add x y = x + y
+import Network.HTTP
 
+jsonURL :: String
+jsonURL = "http://daniel-diaz.github.io/misc/pizza.json"
+
+-- 1. Perform a basic HTTP get request and return the body
+getContent :: String -> IO String
+getContent url = simpleHTTP (getRequest url) >>= getResponseBody
+
+
+addNumbers :: Integer -> Integer -> Integer
+addNumbers x y = x + y
+
+main :: IO ()
 main = putStrLn "hello"
+
