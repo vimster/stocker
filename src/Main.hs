6fdd1e47235a1b2a51ca4d77707c88565887661c
vimster <mr.potsdam@gmx.de>
2015-04-26 19:21:24 +0200

    json parsing

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
@@ -6,7 +5,9 @@ import           Data.Aeson
 import qualified Data.ByteString.Lazy as B
 import qualified Data.Time.Calendar   as T (Day (..), fromGregorian)
 import qualified Data.Time.Format     as F (formatTime)
+import           Network.HTTP.Conduit (simpleHttp)
 -- import qualified Finance.Quote.Yahoo  as Yahoo
+import           Control.Applicative
 import           Network.HTTP
 
 type QuoteSymbol = String
@@ -17,47 +18,43 @@ type QuoteCurrency = Float
 -- | HistoricalQuote reflects the row form of a yahoo historical quote:
 -- Date,Open,High,Low,Close,Volume,Adj Close (taken from the csv itself).
 -- Exported.
-data HistoricalQuote =
-      HistoricalQuote {
-        symbol   :: QuoteSymbol,
-        date     :: T.Day,
-        open     :: QuoteCurrency,
-        high     :: QuoteCurrency,
-        low      :: QuoteCurrency,
-        close    :: QuoteCurrency,
-        adjclose :: QuoteCurrency,
-        volume   :: Int
-        } deriving (Show, Generic)
+data HistoricalQuote = HistoricalQuote {
+        symbol :: QuoteSymbol,
+        date   :: String,
+        open   :: QuoteCurrency,
+        high   :: QuoteCurrency,
+        low    :: QuoteCurrency,
+        close  :: QuoteCurrency
+        -- adjclose :: QuoteCurrency,
+        -- volume   :: Int
+        } deriving (Show)
 
-instance FromJSON HistoricalQuote
+instance FromJSON HistoricalQuote where
+  parseJSON (Object v) = HistoricalQuote <$>
+    (res >>= (.: "Symbol")) <*>
+    (res >>= (.: "Date")) <*>
+    (res >>= (.: "Open")) <*>
+    (res >>= (.: "High")) <*>
+    (res >>= (.: "Low")) <*>
+    (res >>= (.: "Close"))
+    where res = v .: "result"
 
 baseUrl :: String
 baseUrl = "https://query.yahooapis.com/v1/public/yql"
 
-getHistoricalData :: [QuoteSymbol] -> IO [HistoricalQuote]
+-- getHistoricalData :: [QuoteSymbol] -> IO [HistoricalQuote]
 -- getHistoricalData symbols =
 
 -- 1. Perform a basic HTTP get request and return the body
 getContent :: String -> IO String
 getContent url = simpleHTTP (getRequest url) >>= getResponseBody
 
-get :: URI -> IO B.ByteString
-get uri = do
-  let req = Request uri GET [] L.empty
-  resp <- browse $ do
-    request req
-  return $ rspBody $ snd resp
-
-getJSON :: IO B.ByteString
-getJSON = simpleHttp jsonURL
-
-addNumbers :: Integer -> Integer -> Integer
-addNumbers x y = x + y
-
--- vorVier :: [Num] -> [Num]
--- vorVier [] = [0]
--- vorVier (x:xs) = filter (<4)
+getJSON :: String -> IO B.ByteString
+getJSON = simpleHttp
 
 main :: IO ()
-main = putStrLn "hello"
+main = do
+  content <- getJSON baseUrl
+  let Just historicalData = decode content :: Maybe [HistoricalQuote]
+  putStrLn "hello"
 
