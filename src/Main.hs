5d8a4fdd9062161c54ab2a5c59d4d0e0e8274463
vimster <mr.potsdam@gmx.de>
2015-04-27 18:08:15 +0200

    parse json

--- src/Main.hs
+++ src/Main.hs
@@ -2,22 +2,29 @@
 module Main where
 
 import           Data.Aeson
-import qualified Data.ByteString.Lazy as B
-import qualified Data.Time.Calendar   as T (Day (..), fromGregorian)
-import qualified Data.Time.Format     as F (formatTime)
-import           Network.HTTP.Conduit (simpleHttp)
+import qualified Data.ByteString.Lazy       as B
+import qualified Data.ByteString.Lazy.Char8 as BS
+import qualified Data.Time.Calendar         as T (Day (..), fromGregorian)
+import qualified Data.Time.Format           as F (formatTime)
+import           Network.HTTP.Conduit       (simpleHttp)
 -- import qualified Finance.Quote.Yahoo  as Yahoo
 import           Control.Applicative
-import           Data.List            (intercalate)
+import           Data.List                  (intercalate)
 import           Data.Time
 import           Network.HTTP
-import qualified Network.URI.Encode   as Enc
-import           System.Locale        (defaultTimeLocale)
+import qualified Network.URI.Encode         as Enc
+import           System.Locale              (defaultTimeLocale)
+
+testFrom = fromGregorian 2015 04 04
+testTo = fromGregorian 2015 04 08
+testQuotes = ["YHOO"]
 
 type QuoteSymbol = String
 
 -- | Float is not an fully appropriate currency type, beware. Exported.
 type QuoteCurrency = Float
+
+newtype QuoteList = QuoteList [HistoricalQuote] deriving (Show)
 
 -- | HistoricalQuote reflects the row form of a yahoo historical quote:
 -- Date,Open,High,Low,Close,Volume,Adj Close (taken from the csv itself).
@@ -33,18 +40,24 @@ data HistoricalQuote = HistoricalQuote {
         -- volume   :: Int
         } deriving (Show)
 
+instance FromJSON QuoteList where
+  parseJSON (Object v) =
+    QuoteList <$> (res >>= (.: "quote"))
+    where res = (v .: "query") >>= (.: "results")
+
 instance FromJSON HistoricalQuote where
   parseJSON (Object v) = HistoricalQuote <$>
-    (res >>= (.: "Symbol")) <*>
-    (res >>= (.: "Date")) <*>
-    (res >>= (.: "Open")) <*>
-    (res >>= (.: "High")) <*>
-    (res >>= (.: "Low")) <*>
-    (res >>= (.: "Close"))
-    where res = v .: "result"
+    (v .: "Symbol") <*>
+    (v .: "Date") <*>
+    (v .: "Open") <*>
+    (v .: "High") <*>
+    (v .: "Low") <*>
+    (v .: "Close")
+    -- where res = (v .: "query") >>= (.: "results") >>= (.: "quote")
+  -- parseJSON _ = mzero
 
 baseUrl :: String
-baseUrl = "https://query.yahooapis.com/v1/public/yql"
+baseUrl = "http://query.yahooapis.com/v1/public/yql"
 
 -- getHistoricalData :: [QuoteSymbol] -> IO [HistoricalQuote]
 -- getHistoricalData symbols =
@@ -62,15 +75,21 @@ buildHistoricalDataQuery from to symbols =
       \where symbol in (" ++ symbolsFormatted ++ ") and \
       \startDate = '" ++ dbFormat from ++ "' and \
       \endDate = '" ++ dbFormat to ++ "'"
+    env = "&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
   in
-    baseUrl ++ "?q=" ++ query ++ "&format=json"
+    baseUrl ++ "?q=" ++ Enc.encode query ++ "&format=json" ++ env
 
 getJSON :: String -> IO B.ByteString
 getJSON = simpleHttp
 
+testJson = BS.pack "{\"query\":{\"results\":{\"quote\":[{\"Symbol\":\"YHOO\",\"Date\":\"2015-04-24\",\"Open\":\"43.73\",\"High\":\"44.71\",\"Low\":\"43.69\",\"Close\":\"44.52\",\"Volume\":\"11267500\",\"Adj_Close\":\"44.52\"}]}}}"
+
 main :: IO ()
 main = do
-  content <- getJSON baseUrl
-  let Just historicalData = decode content :: Maybe [HistoricalQuote]
-  putStrLn "hello"
+  content <- getJSON $ buildHistoricalDataQuery testFrom testTo testQuotes
+  print content
+  let historicalData = eitherDecode content :: Either String [HistoricalQuote]
+  case historicalData of
+    Left error -> putStrLn error
+    Right ps -> print ps
 
