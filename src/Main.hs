922d3cbaab1762828caaa058c1992eaf95b5962c
vimster <mr.potsdam@gmx.de>
2015-05-02 16:28:26 +0200

    update email notification

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import           Control.Applicative
+import           Control.Monad
 import           Data.Configurator
 import           Data.Configurator.Types     (Config)
 import qualified Data.Function               as Func (on)
 import           Data.List                   (intercalate, maximumBy)
 import qualified Data.Map                    as Map
+import qualified Data.Text.Lazy              as T
 import           Data.Time
 import           Network.HaskellNet.SMTP.SSL as SMTP
 import qualified Yahoofinance                as Yahoo
@@ -26,7 +29,7 @@ isQuoteOfInterest list =
   let maximumPrice = maximum $ map Yahoo.high list
       latestData = maximumBy (compare `Func.on` Yahoo.date) list
       minimumPrice = Yahoo.low latestData
-  in minimumPrice < maximumPrice * 0.8
+  in minimumPrice < maximumPrice * 1.8
 
 stockInfoUrl :: Yahoo.QuoteSymbol -> String
 stockInfoUrl symbol = "http://finance.yahoo.com/q?s=" ++ symbol
@@ -37,24 +40,26 @@ loadConfig = load [Required "appconfig.cfg"]
 host :: String
 host = "smtp.zoho.com"
 
-sendEmail :: String -> String -> String-> Yahoo.QuoteMap -> IO ()
-sendEmail username password receiver quotes = doSMTPSTARTTLS host $ \conn -> do
+headline :: String
+headline = "Folgende Kurse sind in der letzten Woche um mehr als 20% gesunken:\n\n"
+
+sendEmail :: String -> String -> [String]-> Yahoo.QuoteMap -> IO ()
+sendEmail username password receivers quotes = doSMTPSTARTTLS host $ \conn -> do
     authSucceed <- SMTP.authenticate LOGIN username password conn
     if authSucceed
       then print "Authentication error."
-      else sendPlainTextMail receiver username subject body conn
-  where subject = "Achtung! Gefallene Kurse"
-        body    = "test" -- intercalate "\n" $ map (\q -> Yahoo.symbol q ++ ": " ++ stockInfoUrl (Yahoo.symbol q)) quotes
+      else mapM_ (\r -> sendPlainTextMail r username subject body conn) receivers
+  where subject = "Gefallene Kurse"
+        body    =   T.pack $ (headline++) $ intercalate "\n" $ map (\q -> q ++ ": " ++ stockInfoUrl q) (Map.keys quotes)
 
 main :: IO ()
 main = do
-  today <- fmap utctDay getCurrentTime
+  yesterday <- addDays (-1) <$> fmap utctDay getCurrentTime
   config <- loadConfig
   username <- require config "email_user" :: IO String
   password <- require config "email_password" :: IO String
   receiver <- require config "email_receiver" :: IO [String]
-  print receiver
-  historicalData <- Yahoo.getHistoricalData testQuotes (addDays (-7) today) today
+  historicalData <- Yahoo.getHistoricalData testQuotes (addDays (-7) yesterday) yesterday
   let dataOfInterest = filterSymbolsOfInterest historicalData
   print dataOfInterest
-  sendEmail username password (head receiver) dataOfInterest
+  sendEmail username password receiver dataOfInterest
